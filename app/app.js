91c2c8bd3617226c1e373fd03b2bb85364c06bd3
Diego Armando Catalan <diego@drombit.com>
2016-03-25 01:42:52 -0500

    Se añadieron Settings más settings, se añadio la posibilidad de ver y configurar el Iva

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 var hoursConverter = (function() {
     var s, _root;
+    var db = localStorage;
+
 
     // Private Function
     function getTime(time) {
-        // Write the value of the input time
+        // Get the value of the input time
         // if the string have the character : this return and array 
         // with 2 elements [0] for hours and [1] for minutes 
         var pattern = /(\d+)|(\d+)/g; 
@@ -22,28 +24,52 @@ var hoursConverter = (function() {
         }
     }
 
+    // Private function only used in development  
+    function deleteDb() {
+        db.removeItem('Iva');
+        db.removeItem('pricePerHour');
+        db.removeItem('lightTheme');
+    }
+
     // Public Functions and objects
     return {
         settings: {
             pricePerHour: document.getElementById('hourPrice'),
+            ivaPercent: document.getElementById('iva'),
+            ivaBreakDown: document.getElementById('ivaBreakDown'),
             parent: document.querySelector('.converter'),
-            total: document.querySelector('.total'),
+            total: document.getElementById('total'),
+            totalIva: document.getElementById('totalIva'),
             time: document.getElementById('time'),
-            moon: document.querySelector('.moon')
+            theme: document.querySelector('.theme'),
+            lightTheme: document.getElementById('lightTheme'),
+            settingsIcon: document.querySelector('.geomicon'),
+            settingOptions: document.querySelector('.settings__options'),
+            myDocument: document.documentElement
         },
         init: function() {
             _root = this;
              s = _root.settings;
 
             _root.getPrice();
+
+            _root.loadSettings();
+            _root.activeSettings();
             _root.changeTheme();
+            _root.updateSettings();
         },
         getPrice: function() {
             s.parent.addEventListener('input', function(e){
                 if (e.target !== e.currentTarget) {
                     var totalMinutes = _root.convertToMinutes(s.time.value);
                     var granTotal = (totalMinutes * s.pricePerHour.value) / 60;
+                    var iva = (s.ivaPercent.value/100);
+                    var granTotalIva = granTotal * (iva + 1);
+
+                    // Update Values
                     s.total.innerHTML = "$" + granTotal.toFixed(2);
+                    s.ivaBreakDown.innerHTML = "($" + granTotal * iva + ")";
+                    s.totalIva.innerHTML = "$" + granTotalIva.toFixed(2);
                 }           
             });
         },
@@ -57,12 +83,62 @@ var hoursConverter = (function() {
 
             return totalMinutes;
         },
-        changeTheme: function() {
-            s.moon.addEventListener('click', function() {
-              this.classList.toggle('moon');
-              s.parent.classList.toggle('converter--sun')
+        activeSettings: function() {
+            s.settingsIcon.addEventListener('click', function(e) {
+                s.settingOptions.classList.toggle('settings__options--active');
             });
+        },
+        changeTheme: function() {
+            s.theme.addEventListener('click', function() {
+              this.classList.toggle('sun');
+              s.parent.classList.toggle('converter--sun');
+
+              if (s.parent.classList.length > 1) {
+                s.lightTheme.checked = true;
+                _root.updateSettings();
+              } else {
+                s.lightTheme.checked = false;
+                _root.updateSettings();
+              }
+            });
+        },
+        loadSettings: function() {
+            var getPricePerHour = db.getItem('pricePerHour');
+            var getIva = db.getItem('Iva');
+            var getTheme = db.getItem('lightTheme');
+
+            // Set the default settings of the app
+            // used when the user open the app for the first time
+            if (getTheme === null) {
+                var defaultPricePerHour = db.setItem('pricePerHour', '250');
+                var defaultIva = db.setItem('Iva', '16');
+                var defaultTheme = db.setItem('lightTheme', 'false');
+                s.pricePerHour.value = db.getItem('pricePerHour');
+                s.ivaPercent.value = db.getItem('Iva'); 
+                s.lightTheme.checked = db.getItem('lightTheme'); 
+            } else {
+                s.pricePerHour.value = getPricePerHour;
+                s.ivaPercent.value = getIva;
+                if (getTheme === 'true') {
+                    s.parent.classList.add('converter--sun');
+                    s.theme.classList.add('sun');
+                    s.lightTheme.checked = true;
+                }
+                 
+            }
+        },
+        updateSettings: function() {
+            s.settingOptions.addEventListener('input', function(e) {
+                if (e.target !== e.currentTarget) {
+                    db.setItem('pricePerHour', s.pricePerHour.value);
+                    db.setItem('Iva', s.ivaPercent.value);
+                }
+            });
+
+            // Update the checked value of the theme
+            db.setItem('lightTheme', s.lightTheme.checked);
         }
+
     };
 }());
 
